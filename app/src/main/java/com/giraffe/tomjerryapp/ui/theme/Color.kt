5c8c02ae4701da0b32fc35ca8ae350868bb78e1a
Mohannad Ahmed <mohannad199344@gmail.com>
2025-05-31 19:03:28 +0300

    Add new colors and typography styles and icons and images

--- app/src/main/java/com/giraffe/tomjerryapp/ui/theme/Color.kt
+++ app/src/main/java/com/giraffe/tomjerryapp/ui/theme/Color.kt
@@ -9,3 +9,11 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+
+val darkGray = Color(0xFF1F1F1E)
+val gray = Color(0xFF969799)
+val lightGray = Color(0xFFA5A6A4)
+val black = Color(0xFF121212)
+val blue = Color(0xFF03578A)
+val lightBlue = Color(0xFFD0E5F0)
--- app/src/main/java/com/giraffe/tomjerryapp/ui/theme/Type.kt
+++ app/src/main/java/com/giraffe/tomjerryapp/ui/theme/Type.kt
@@ -1,20 +1,77 @@
 package com.giraffe.tomjerryapp.ui.theme
 
 import androidx.compose.material3.Typography
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.sp
 
 // Set of Material typography styles to start with
 val Typography = Typography(
+    displaySmall = TextStyle(
+        fontFamily = FontFamily.Default,
+        color = Color.White,
+        fontSize = 10.sp,
+        textAlign = TextAlign.Center,
+        fontWeight = FontWeight.W500
+    ),
+    bodySmall = TextStyle(
+        fontFamily = FontFamily.Default,
+        color = gray,
+        fontSize = 12.sp,
+        textAlign = TextAlign.Center,
+        fontWeight = FontWeight.W400
+    ),
+    bodyMedium = TextStyle(
+        fontFamily = FontFamily.Default,
+        color = black.copy(alpha = 0.6f),
+        fontSize = 14.sp,
+        lineHeight = 20.sp,
+        letterSpacing = 0.5.sp,
+        fontWeight = FontWeight.W500
+    ),
     bodyLarge = TextStyle(
         fontFamily = FontFamily.Default,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp,
         lineHeight = 24.sp,
         letterSpacing = 0.5.sp
-    )
+    ),
+
+    labelSmall = TextStyle(
+        fontFamily = FontFamily.Default,
+        color = blue,
+        fontSize = 12.sp,
+        textAlign = TextAlign.Center,
+        fontWeight = FontWeight.W500
+    ),
+
+    labelMedium = TextStyle(
+        fontFamily = FontFamily.Default,
+        color = Color.White,
+        fontSize = 16.sp,
+        textAlign = TextAlign.Center,
+        fontWeight = FontWeight.W500
+    ),
+
+    titleSmall = TextStyle(
+        fontFamily = FontFamily.Default,
+        color = Color.White,
+        fontSize = 18.sp,
+        fontWeight = FontWeight.W600,
+        lineHeight = 16.sp,
+        letterSpacing = 0.5.sp
+    ),
+    titleMedium = TextStyle(
+        fontFamily = FontFamily.Default,
+        color = darkGray,
+        fontSize = 20.sp,
+        fontWeight = FontWeight.W500
+    ),
+
+
     /* Other default text styles to override
     titleLarge = TextStyle(
         fontFamily = FontFamily.Default,
