1b5b52be3ead94fddf95a6d7f92e53ff257ffeae
Mohannad Ahmed <mohannad199344@gmail.com>
2025-06-04 10:42:29 +0300

    Add Google Fonts support and update typography styles for improved text rendering

--- app/src/main/java/com/giraffe/tomjerryapp/ui/theme/Type.kt
+++ app/src/main/java/com/giraffe/tomjerryapp/ui/theme/Type.kt
@@ -4,10 +4,26 @@ import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.googlefonts.GoogleFont
 import androidx.compose.ui.unit.sp
 import com.giraffe.tomjerryapp.R
 
 // Set of Material typography styles to start with
+
+val provider = GoogleFont.Provider(
+    providerAuthority = "com.google.android.gms.fonts",
+    providerPackage = "com.google.android.gms",
+    certificates = R.array.com_google_android_gms_fonts_certs
+)
+
+val fontName = GoogleFont("Roboto")
+//val fontName = GoogleFont("IBM Plex Sans Arabic")
+//val fontName = GoogleFont("Arvo")
+//val fontName = GoogleFont("IBM Plex Sans")
+
+val fontFamily = FontFamily(androidx.compose.ui.text.googlefonts.Font(googleFont = fontName, fontProvider = provider))
+//val fontFamily = FontFamily.Default
+
 val IbmPlexSansArabicFamily = FontFamily(
     Font(R.font.thin, FontWeight.Thin),
     Font(R.font.light, FontWeight.Light),
@@ -18,68 +34,68 @@ val IbmPlexSansArabicFamily = FontFamily(
     Font(R.font.bold, FontWeight.Bold),
 )
 val medium10 = TextStyle(
-    fontFamily = FontFamily.Default,
+    fontFamily = fontFamily,
     fontSize = 10.sp,
     fontWeight = FontWeight.Medium,
 )
 val medium12 = TextStyle(
-    fontFamily = FontFamily.Default,
+    fontFamily = fontFamily,
     fontSize = 12.sp,
     fontWeight = FontWeight.Medium,
 )
 val medium14 = TextStyle(
-    fontFamily = FontFamily.Default,
+    fontFamily = fontFamily,
     fontSize = 14.sp,
     fontWeight = FontWeight.Medium,
 )
 val medium16 = TextStyle(
-    fontFamily = FontFamily.Default,
+    fontFamily = fontFamily,
     fontSize = 16.sp,
     fontWeight = FontWeight.Medium,
 )
 val medium18 = TextStyle(
-    fontFamily = FontFamily.Default,
+    fontFamily = fontFamily,
     fontSize = 18.sp,
     fontWeight = FontWeight.Medium,
 )
 val medium20 = TextStyle(
-    fontFamily = FontFamily.Default,
+    fontFamily = fontFamily,
     fontSize = 20.sp,
     fontWeight = FontWeight.Medium,
 )
 
 val regular12 = TextStyle(
-    fontFamily = FontFamily.Default,
+    fontFamily = fontFamily,
     fontSize = 12.sp,
     fontWeight = FontWeight.Normal,
 )
 val regular14 = TextStyle(
-    fontFamily = FontFamily.Default,
+    fontFamily = fontFamily,
     fontSize = 14.sp,
     fontWeight = FontWeight.Normal,
 )
 val semiBold14 = TextStyle(
-    fontFamily = FontFamily.Default,
+    fontFamily = fontFamily,
     fontSize = 14.sp,
     fontWeight = FontWeight.SemiBold,
 )
 val semiBold16 = TextStyle(
-    fontFamily = FontFamily.Default,
+    fontFamily = fontFamily,
     fontSize = 16.sp,
     fontWeight = FontWeight.SemiBold,
 )
 val semiBold18 = TextStyle(
-    fontFamily = FontFamily.Default,
+    fontFamily = fontFamily,
     fontSize = 18.sp,
     fontWeight = FontWeight.SemiBold,
 )
 val semiBold20 = TextStyle(
-    fontFamily = FontFamily.Default,
+    fontFamily = fontFamily,
     fontSize = 20.sp,
     fontWeight = FontWeight.SemiBold,
 )
 val bold20 = TextStyle(
-    fontFamily = FontFamily.Default,
+    fontFamily = fontFamily,
     fontSize = 20.sp,
     fontWeight = FontWeight.Bold,
 )
